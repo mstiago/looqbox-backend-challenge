fa936970a65f51f82717787ac75ddddfdcf42cee
Tiago Matos <tiagos1401@gmail.com>
2021-08-15 18:28:36 -0300

    Adding controller

---
+++ src/main/java/com/tiago/pokeapi/controllers/PokemonController.java
@@ -0,0 +1,26 @@
+package com.tiago.pokeapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import com.tiago.pokeapi.models.Pokemon;
+import com.tiago.pokeapi.services.PokemonService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/pokemons")
+@RestController
+public class PokemonController {
+
+    @Autowired
+    private PokemonService pokemonService;
+
+    @GetMapping
+    public List<Pokemon> getPokemon(@RequestParam("q") String query ) {
+        return pokemonService.GetPokemons(query , null);
+    }
+}
